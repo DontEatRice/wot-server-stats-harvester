94b538593bb774c3b4ae1186b72d2092bfecb862
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2021-03-14 13:25:05 +0100

    fixed bug in dailyStats implementation

--- dailyStats.js
+++ dailyStats.js
@@ -7,6 +7,12 @@ const servers = [
     { schema: Stats.ASIA, name: "asia" }
   ];
 
+const findDay = async () => {
+    const find = await servers[0].schema.find().sort({date: -1}).limit(1);
+    let date = find[0].date;
+    return date.setDate(date.getDate()-1);   
+}
+
 const getAvg = (data) => {
     let sum = 0;
     for (const stats of data) {
@@ -18,20 +24,21 @@ const getAvg = (data) => {
 } 
 
 const dayAvg = async () => {
-    const today = new Date();
-    today.setHours(0, 1, 0, 0);
-    const yester = new Date(today);
-    yester.setHours(yester.getHours()-24)
+    const day = await findDay();
+    const start = new Date(day)
+    start.setHours(1, 0, 0, 0);
+    const stop = new Date(start);
+    stop.setHours(start.getHours()+24);
     let dataset = [];
     for(const server of servers) {
         try {
-            const data = await server.schema.find({date: {$gt: yester.toISOString(), $lt: today.toISOString()}}).exec();
+            const data = await server.schema.find({date: {$gte: start.toISOString(), $lt: stop.toISOString()}}).exec();
             dataset.push({ name: server.name, players: getAvg(data)});
         } catch (e) {
             return{ status: 'error', msg: 'finding error' };
         }
     }
-    const toPush = new Stats.DAY({servers: dataset, date: yester});
+    const toPush = new Stats.DAY({servers: dataset, date: start});
     const errs = toPush.validateSync();
     if (errs)
         console.log(errs);
